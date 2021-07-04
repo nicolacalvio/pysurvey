5383b9512e8236037dcf6330476f908633d159a0
nicola calvio <info@nicolacalvio.com>
2021-07-04 19:59:52 +0200

    aggiunto supporto utente loggato

--- pysurvey/app/home/routes.py
+++ pysurvey/app/home/routes.py
@@ -9,9 +9,11 @@ home = Blueprint('home', __name__)
 @home.route('/', methods=['GET', 'POST'])
 def homepage():
     return render_template('home.html', title='Home')
+    #return render_template('home.html', title='Home', signed_in='True')
 
 
 @home.route('/about')
 def about():
     return render_template('about.html', title='About')
+    #return render_template('about.html', title='About', signed_in='True')
 
