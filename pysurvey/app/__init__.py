2b1debbedd57fefee0f2c6bd057034af2af53554
nicola calvio <info@nicolacalvio.com>
2021-08-28 17:43:33 +0200

    stabilità e integrità del db migliorata

--- pysurvey/app/__init__.py
+++ pysurvey/app/__init__.py
@@ -16,7 +16,7 @@ import csv
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'nosql'
 app.config.from_object(DevelopmentConfig)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:bitnami@pysurvey.ddns.net:5432/pysurvey"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:bitnami@localhost:5432/pysurvey"
 from model import User, Survey, Domande, Risposte, RisposteUtenti, Statistiche, db
 home = Blueprint('home', __name__)
 
--- pysurvey/model.py
+++ pysurvey/model.py
@@ -6,16 +6,16 @@ db = SQLAlchemy(app)  # inizializzazione del db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), unique=True)
-    email = db.Column(db.String(200))
-    password = db.Column(db.String(200))
+    email = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     immagine = db.Column(db.String(200))
     nazionalita = db.Column(db.String(200), default="Italiana")
 
 
 class Survey(db.Model):
     idSurvey = db.Column(db.Integer, primary_key=True)  # sono chiavi esterne
-    idUser = db.Column(db.Integer, ForeignKey('user.id'))
-    titolo = db.Column(db.String(80))
+    idUser = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
+    titolo = db.Column(db.String(80), nullable=False)
 
     # property utile in fase di serializzaizone in json
     @property
@@ -29,21 +29,21 @@ class Survey(db.Model):
 
 
 class Domande(db.Model):
-    idSurvey = db.Column(db.Integer, ForeignKey('survey.idSurvey'))  # sono chiavi esterne
+    idSurvey = db.Column(db.Integer, ForeignKey('survey.idSurvey'), nullable=False)  # sono chiavi esterne
     idDomanda = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(200))
-    singola = db.Column(db.Boolean)
+    question = db.Column(db.String(200), nullable=False)
+    singola = db.Column(db.Boolean, nullable=False)
 
 
 class Risposte(db.Model):
     idRisposta = db.Column(db.Integer, primary_key=True)
-    idDomanda = db.Column(db.Integer, ForeignKey('domande.idDomanda'))
-    risposta = db.Column(db.String(80))
+    idDomanda = db.Column(db.Integer, ForeignKey('domande.idDomanda'), nullable=False)
+    risposta = db.Column(db.String(80), nullable=False)
 
 
 class RisposteUtenti(db.Model):
-    idRisposta = db.Column(db.Integer, ForeignKey('risposte.idRisposta'))
-    idDomanda = db.Column(db.Integer, ForeignKey('domande.idDomanda'))
+    idRisposta = db.Column(db.Integer, ForeignKey('risposte.idRisposta'), nullable=False)
+    idDomanda = db.Column(db.Integer, ForeignKey('domande.idDomanda'), nullable=False)
     idUtente = db.Column(db.Integer, ForeignKey('user.id'))
     idRispostaUtente = db.Column(db.Integer, primary_key=True)
 
